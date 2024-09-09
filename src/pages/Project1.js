fdfcdc9a2f4455b116a18e10627628a8d87de1ac
bwoodward523 <bwoodward@ycp.edu>
2024-09-09 14:52:12 -0400

    a

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -9,9 +9,9 @@ function Project1() {
         <div className="robotbg min-h-screen bg-gray-100 flex flex-col items-center justify-start p-10">
             <h1 className="text-4xl font-bold text-blue-600">{projects[0].title}</h1>
             <p className="mt-4 text-gray-100" >{projects[0].description}</p>
-            {<iframe title="robot" frameborder="0" src="https://itch.io/embed-upload/11249606?color=333333" allowfullscreen="" width="960" height="660" >
+            {/*<iframe title="robot" frameborder="0" src="https://itch.io/embed-upload/11249606?color=333333" allowfullscreen="" width="960" height="660" >
                 <a href="https://edon-games.itch.io/the-robot-who-baked-the-world">Play The Robot Who Baked The World on itch.io</a>
-            </iframe>}
+            </iframe>*/}
         </div>
     );
 }
