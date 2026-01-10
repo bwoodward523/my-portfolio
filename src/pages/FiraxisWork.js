47b984f2b6ab193497604a9cc52a6b90dde1d8d8
bwoodward523 <bwoodward@ycp.edu>
2026-01-10 17:02:04 -0500

    Articlizing Firaxis nad JCI

--- src/pages/FiraxisWork.js
+++ src/pages/FiraxisWork.js
@@ -2,50 +2,55 @@ import React from "react";
 import img1 from "../images/InternsFiraxisHappy.jpg";
 import img2 from "../images/HappySethBrandonFiraxis.jpg";
 import firaxislogo from "../images/firaxis.png";
+import ArticleLayout, { ArticleHeader, ArticleSection, ArticleParagraph, ArticleFigure } from '../components/ArticleLayout'
 
 function FiraxisWork() {
     return (
-        <div className="bg-white">
-            <p className="text-center p-2 pt-20 text-5xl font-bold ">
-                <img className="inline" alt="Firaxis logo" src={firaxislogo} />
-                <br /> Engineering Intern <br />
-            </p>
-            <p className="text-center pt-6 text-3xl font-bold mb-6">Sparks, MD</p>
+        <ArticleLayout>
+            <ArticleHeader 
+                logo={firaxislogo}
+                logoAlt="Firaxis logo"
+                title="Engineering Intern"
+                subtitle="Sparks, MD"
+            />
 
-            <div className="bg-gray-400 p-8 md:p-24 lg:p-24 text-xl md:text-3xl lg:text-5xl font-bold text-white justify-items-center flex flex-col md:flex-none lg:flex-none items-center">
-                <p className="mx-4 ">
-                    I took the lead in a title called Callisto, it is a multiplayer party game using Unreal Engine 5 and C++. I was responsible for developing the networking and game framework. Meaning I created the game loop, the basic multiplayer functionalities, most of the UI, the basic environment and some fun mechanics.
-                    <br className=""  />
-                    <br />
+            <ArticleSection variant="light">
+                <ArticleParagraph>
+                    At Firaxis Games,I took the lead in a title called Callisto, it is a multiplayer party game using Unreal Engine 5 and C++. I was responsible for developing the networking and game framework. Meaning I created the game loop, the basic multiplayer functionalities, most of the UI, the basic environment and some fun mechanics.
+                </ArticleParagraph>
 
+                <ArticleParagraph>
                     Throughout this process, I honed my teamwork skills by collaborating closely with interns, mentors, and employees from various specializations.
-                </p>
-                <img className="pt-8 inline-block rounded ml-auto mr-auto w-auto lg:w-3/4 h-auto" src={img2} alt="Brandon Woodward" />
-                <br />
-            </div>
+                </ArticleParagraph>
 
-            <div className="p-8 md:p-24 lg:p-24 text-xl md:text-3xl lg:text-5xl font-bold bg-gray-900 text-white flex flex-col items-center">
-                <p className="mx-4">
-                My eagerness to learn from my coworkers allowed me to quickly refine my abilities.
-                <br /><br />
-                Working alongside my fellow interns, we supported each other and proved a formidable development team.
-                <br /><br />
-                In addition to the aforementioned, I adapted to an existing codebase, and I implemented new features for an ongoing project.
-                </p>
-                <img className="rounded pt-6 relative inline-block mr-auto w-auto h-auto" src={img1} alt="Brandon Woodward" />
-            </div>
+                <ArticleFigure 
+                    src={img2}
+                    alt="Brandon Woodward with colleagues at Firaxis"
+                    caption="Me posing with Seth Davis during the internship graduation ceremony."
+                />
+            </ArticleSection>
 
-            {/* <div className="bg-gray-400 text-white">
-                <p className="text-center pt-20 text-5xl font-bold pb-8">Other Work Experience</p>
-                <ul>
-                    <li className="text-center text-2xl pb-4 mr-4 ml-4">&bull; Lifeguard at DRD Pools for three summers: 2019, 2020, and 2021</li>
-                    <li className="text-center text-2xl pb-10 mr-4 ml-4">&bull; Leadership Training Counselor at Beth Tfiloh Camps in 2018</li>
-                </ul>
-            </div> */}
+            <ArticleSection variant="dark">
+                <ArticleParagraph variant="dark">
+                    My eagerness to learn from my coworkers allowed me to quickly refine my abilities.
+                </ArticleParagraph>
 
+                <ArticleParagraph variant="dark">
+                    Working alongside my fellow interns, we supported each other and proved a formidable development team.
+                </ArticleParagraph>
 
-            {/* */}
-        </div>
+                <ArticleParagraph variant="dark">
+                    In addition to the aforementioned, I adapted to an existing codebase, and I implemented new features for an ongoing project.
+                </ArticleParagraph>
+
+                <ArticleFigure 
+                    src={img1}
+                    alt="Firaxis interns together"
+                    caption="The Firaxis intern team at the graduation ceremony."
+                    variant="dark"
+                />
+            </ArticleSection>
+        </ArticleLayout>
     );
 }
 export default FiraxisWork;
--- src/pages/JCIWork.js
+++ src/pages/JCIWork.js
@@ -1,20 +1,36 @@
 import React from "react";
 import jcilogo from '../images/Johnson_Controls.svg.webp'
+import ArticleLayout, { ArticleHeader, ArticleSection, ArticleParagraph } from '../components/ArticleLayout'
 
 function JCIWork() {
     const jciPoster = `${process.env.PUBLIC_URL}/JCIPoster.pdf`;
     
     return (
-        <div className="bg-white">
-            <p className="text-center p-2 pt-20 text-5xl font-bold ">
-                <img className="inline w-1/4 h-1/4 object-scale-down" alt="Johnson Controls logo" src={jcilogo} />
-                <br /> Software Test Automation Engineering Intern <br />
-                <br /> THE TEXT IN THIS PAGE IS PLACEHOLDER TEXT ... THE PAGE IS UNDER CONSTRUCTION
-            </p>
-            <p className="text-center pt-6 text-3xl font-bold mb-6">New Freedom, PA</p>
-            <div className="bg-gray-100 p-8 md:p-16 lg:p-20 flex flex-col items-center">
-                <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-800 mb-8 text-center">Project Poster</h2>
-                <div className="w-full max-w-4xl">
+        <ArticleLayout>
+            <ArticleHeader 
+                logo={jcilogo}
+                logoAlt="Johnson Controls logo"
+                title="Software Test Automation Engineering Intern"
+                subtitle="New Freedom, PA"
+            />
+
+            <ArticleSection variant="light">
+                <ArticleParagraph>
+                    I really cherished the time I spent at Johnson Controls. Dr. Joe Villani is a fantastic mentor and the environment was extremely supportive!
+                </ArticleParagraph>
+
+                <ArticleParagraph>
+                    This was a remarkable opportuntiy to learn and grow as a developer and contribute to a real product in such a meaningful manner!
+                </ArticleParagraph>
+
+                <ArticleParagraph>
+                    I'd also like to stress the scope of my accomplishments in such a short period of time. I was only at the site for three months, two days a week yet I managed to create everything you see in the poster!
+                </ArticleParagraph>
+            </ArticleSection>
+
+            <ArticleSection variant="dark">
+                <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-100 mb-8 text-center">Project Poster</h2>
+                <div className="w-full max-w-4xl mx-auto">
                     <iframe 
                         src={jciPoster}
                         className="w-full h-[600px] md:h-[800px] lg:h-[1000px] border-2 border-gray-300 rounded-lg shadow-lg"
@@ -24,23 +40,10 @@ function JCIWork() {
                         <p className="text-center p-4">Your browser does not support PDFs. <a href={jciPoster} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">Click here to download the PDF</a>.</p>
                     </iframe>
                 </div>
-            </div>
-            {/* <div className="bg-gray-400 p-8 md:p-24 lg:p-24 text-xl md:text-3xl lg:text-5xl font-bold text-white justify-items-center flex flex-col md:flex-none lg:flex-none items-center">
-                <p className="mx-4 ">
+            </ArticleSection>
 
-                </p>
-            </div> */}
-
-            <div className="p-8 md:p-24 lg:p-24 text-xl md:text-3xl lg:text-5xl font-bold bg-gray-900 text-white flex flex-col items-center">
-                <p className="mx-4 ">
-                    I really cherished the time I spent at Johnson Controls. Dr. Joe Villani is a fantastic mentor and the environment was extremely supportive! <br/>  <br/>
-                    This was a remarkable opportuntiy to learn and grow as a developer and contribute to a real product in such a meaningful manner! <br/> <br/>
-                    I'd also like to stress the scope of my accomplishments in such a short period of time. I was only at the site for three months, two days a week yet I managed to create everything you see in the poster!
-                </p>
-            </div>
-
-          
-        </div>
+           
+        </ArticleLayout>
     );
 }
 
